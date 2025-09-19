551a15282e0c4384097f223af5e26c5e30435071
soumelongv-dotcom <soumelongv@gmail.com>
2025-09-20 00:40:17 +0200

    Nouveau message

--- handles.js
+++ handles.js
@@ -1,27 +1,81 @@
 // ./handles.js
 
-// Import des modules nécessaires
 const url = require('url')
 const qs = require('querystring')
+const fs = require('fs')
+const path = require('path')
 
-// Fonction qui gère toutes les requêtes HTTP
+// Liste de réponses aléatoires pour /hello
+const randomResponses = [
+  "Salut !",
+  "Bonjour !",
+  "Hey !",
+  "Hello !"
+]
+
 const serverHandle = function (req, res) {
   const route = url.parse(req.url)
-  const path = route.pathname 
+  const pathName = route.pathname
   const params = qs.parse(route.query)
 
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
+  // ----------------------
+  // Route /explique
+  // ----------------------
+  if (pathName === '/explique') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('<h1>Page Explique</h1>')
+    res.write('<ul>')
+    res.write('<li><a href="/hello?name=Guy">/hello?name=Guy</a></li>')
+    res.write('<li><a href="/about">/about</a></li>')
+    res.write('</ul>')
+    res.end()
+    return
   }
 
+  // ----------------------
+  // Route /hello
+  // ----------------------
+  if (pathName === '/hello') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    if ('name' in params) {
+      if (params['name'].toLowerCase() === 'guy') {
+        res.write('<p>Bonjour, je suis Guy Soumelong, étudiant en 4ème année.</p>')
+      } else {
+        const random = randomResponses[Math.floor(Math.random() * randomResponses.length)]
+        res.write(`<p>${random} ${params['name']}!</p>`)
+      }
+    } else {
+      res.write('<p>Hello anonymous!</p>')
+    }
+    res.end()
+    return
+  }
+
+  // ----------------------
+  // Route /about
+  // ----------------------
+  if (pathName === '/about') {
+    const jsonPath = path.join(__dirname, 'content', 'about.json')
+    if (fs.existsSync(jsonPath)) {
+      const aboutContent = require(jsonPath)
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.write(JSON.stringify(aboutContent, null, 2))
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.write('404 Not Found - JSON manquant')
+    }
+    res.end()
+    return
+  }
+
+  // ----------------------
+  // Route par défaut (404)
+  // ----------------------
+  res.writeHead(404, { 'Content-Type': 'text/plain' })
+  res.write('404 Not Found')
   res.end()
 }
 
-// Export de la fonction pour l’utiliser dans index.js
 module.exports = {
   serverHandle
 }
--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 // ./index.js
 
-console.log("Hello Node.js!");
+console.log("Hello Node.js !")
 
-
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
-// Import du module http
 const http = require('http')
-
-// Import du module handles.js
 const handles = require('./handles')
 
-// Création et démarrage du serveur sur le port 8080
 http
   .createServer(handles.serverHandle)
   .listen(8080, () => {
     console.log('Serveur démarré sur http://localhost:8080')
   })
-
-
-// ./index.js
-const http = require('http')
-const handles = require('./handles')
-
-http
-.createServer(handles.serverHandle)
-.listen(8080)
