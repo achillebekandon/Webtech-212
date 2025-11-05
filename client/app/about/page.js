1d7cf586f0b1e043a595e0d77691009adb6e5001
achillebekandon <achillemiguelduval.bekandon@edu.ece.fr>
2025-11-05 09:27:58 +0100

    Import du dossier depuis la branche Achille

---
+++ client/app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>À propos</h1>
+      <p>Je suis un étudiant passionné par le développement web et l’IA.</p>
+    </main>
+  );
+}
---
+++ client/app/contacts/page.js
@@ -0,0 +1,8 @@
+export default function Contacts() {
+  return (
+    <main>
+      <h1>Contactez-moi</h1>
+      <p>Email : contact@monblog.com</p>
+    </main>
+  );
+}
