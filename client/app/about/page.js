f10a1990d79337bb1e98d9836a1365409d8d6d3a
soumelongv-dotcom <soumelongv@gmail.com>
2025-11-09 23:24:37 +0100

    feat: Déplacement des anciens lab dans Archives

--- client/app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main>
-      <h1>À propos</h1>
-      <p>Je suis un étudiant passionné par le développement web et l’IA.</p>
-    </main>
-  );
-}
--- client/app/contacts/page.js
+++
@@ -1,8 +0,0 @@
-export default function Contacts() {
-  return (
-    <main>
-      <h1>Contactez-moi</h1>
-      <p>Email : contact@monblog.com</p>
-    </main>
-  );
-}
